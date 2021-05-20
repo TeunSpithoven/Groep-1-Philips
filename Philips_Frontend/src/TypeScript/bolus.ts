46f69e07510ff74cd6ae3d017dcaac574c55e0f3
Teun Spithoven <74899257+TeunSpithoven@users.noreply.github.com>
2021-05-20 09:50:59 +0200

    fixed front-end

--- Philips_Frontend/src/TypeScript/bolus.ts
+++ Philips_Frontend/src/TypeScript/bolus.ts
@@ -7,14 +7,14 @@ const button = document.getElementById("button");
 
 function GetBolusCalculation(weight: number, carbsOfMeal: number) {
     var http = new XMLHttpRequest();
-    // var params = 'weight=' + weight + "&carbsOfMeal=" + carbsOfMeal;
-    // var url = 'http://localhost:3000/bolus/?' + params;
-    var url = 'http://localhost:3000/bolus/';
+    var params = 'weight=' + weight + "&carbsOfMeal=" + carbsOfMeal;
+    var url = 'http://localhost:3000/bolus/?' + params;
+    //var url = 'http://localhost:3000/bolus/';
     http.open('GET', url);
 
     //Send the proper header information along with the request
-    // http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    //http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
     http.onreadystatechange = function () {//Call a function when the state changes.
         if (http.readyState == 4 && http.status == 200) {
@@ -29,9 +29,9 @@ function GetBolusCalculation(weight: number, carbsOfMeal: number) {
             }
         }
     }
-    http.send(JSON.stringify({ "weight": `${weight}`, "carbsOfMeal": `${carbsOfMeal}` }));
+    //http.send(JSON.stringify({ "weight": `${weight}`, "carbsOfMeal": `${carbsOfMeal}` }));
 
-    // http.send(params);
+    http.send(params);
 }
 
 window.onload = function () {
